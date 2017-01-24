fd23a834ccf1e0685cbb58361ef742985380575a
Luke Todd <luke.todd@plus.net>
2017-01-24 14:05:51 +0000

    Removal of logo and addition of some fonts

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,43 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-      work: {
-          preactImageGallery: {
-              title: 'Preact Image Gallery',
+      articles: {
+          articleOne: {
+              title: 'Article Number One',
               url: '#',
-              description: 'Photo gallery using Preact, the Imgur API and some lazy loading.',
-              screengrab: '/images/gallerysc.jpg',
+              description: `Lorem ipsum dolor sit amet, Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+              screengrab: 'https://www.fillmurray.com/700/240',
               mobile_screengrab: '#'
           },
-          getgovgrants: {
-              title: 'Colour List Tool',
+          articleTwo: {
+              title: 'Article Number Two',
               url: '#',
-              description: 'A React & Firebase tool to pick and save colour palettes.',
+              description: `Lorem ipsum dolor sit amet, Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
               screengrab: 'https://www.fillmurray.com/700/240',
               mobile_screengrab: '#'
           }
-      }
+      },
+      colours: [
+        "#F44336",
+        "#E91E63",
+        "#9C27B0",
+        "#673AB7",
+        "#3F51B5",
+        "#2196F3",
+        "#03A9F4",
+        "#00BCD4",
+        "#009688",
+        "#4CAF50",
+        "#8BC34A",
+        "#CDDC39",
+        "#FFEB3B",
+        "#FFC107",
+        "#FF9800",
+        "#FF5722",
+        "#795548",
+        "#9E9E9E",
+        "#607D8B"
+      ]
    });
 });
 
